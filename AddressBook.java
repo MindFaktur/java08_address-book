093958126396389d45b8a8c4ff19e3324fb84dda
Vishwanath <vishwanathiti828@gmail.com>
2021-12-19 21:06:17 +0530

    added interfaces and ability to edit all details

--- AddressBook.java
+++ AddressBook.java
@@ -4,22 +4,7 @@ import java.util.HashMap;
 public class AddressBook{
 	Scanner scan = new Scanner(System.in);
 	public static void main( String[] args ) {
-		int option = 0;
-		while (option != 2) {
-			System.out.println(" Press 1) Add Contact details 2) quit 3) change address ");
-			AddressBook obj = new AddressBook();
-			ContactData contactDataObject = new ContactData();
-			option = obj.scan.nextInt();
-			switch ( option ){
-				case 1: contactDataObject.addContact();
-						System.out.println(ContactData.contactsMap);
-						break;
-				case 3: contactDataObject.changeAddress();
-						System.out.println(ContactData.contactsMap);
-						break;
-			}
-		}
-		System.out.println(ContactData.contactsMap);
+		Menu.menu();
 	}
 }
 
